6c2da9ea9dc1e32a5233c0365f2767a07427b0d5
kilingzhang <17439300+kilingzhang@users.noreply.github.com>
2024-01-11 00:58:11 +0800

    feat(stoverride): 更新raycast API匹配和处理逻辑 (#main)
    - 更新raycast.stoverride文件，增加API匹配规则和脚本参数
    - 修改raycast.js脚本，增强API响应处理功能
    - 新增cloud_sync和AI模型支持
    
    BREAKING CHANGE: 增加了新的API端点匹配和处理逻辑，可能需要客户端更新

--- raycast.js
+++ raycast.js
@@ -1,10 +1,11 @@
 let request = $request
 let response = $response
+let url = request.url
 console.log("before request:\n" + JSON.stringify(request))
 console.log("before response:\n" + JSON.stringify(response))
 try {
     response.body = JSON.parse(response.body)
-    switch (request.url) {
+    switch (url) {
         case "https://backend.raycast.com/api/v1/me":
             response.body['eligible_for_pro_features'] = true
             response.body['has_active_subscription'] = true
@@ -15,6 +16,7 @@ try {
             response.body['eligible_for_application_settings'] = true
             response.body['publishing_bot'] = true
             response.body['has_pro_features'] = true
+            response.body['eligible_for_cloud_sync'] = true
             response.body['has_better_ai'] = true
             response.body['can_upgrade_to_pro'] = true
             response.body['admin'] = true
@@ -25,14 +27,53 @@ try {
             response.body['trial_limits']['quicklinks_limit'] = 999
             response.body['trial_limits']['snippets_limit'] = 999
             break;
+        case "https://backend.raycast.com/api/v1/ai/models":
+            response.body['models'] = [
+                {
+                    "id": "gemini-pro",
+                    "model": "gemini-pro",
+                    "name": "Gemini Pro",
+                    "provider": "google",
+                    "provider_name": "Google",
+                    "requires_better_ai": true,
+                    "features": [],
+                },
+            ]
+            response.body['models'] = [
+                {
+                    "id": "openai-gpt-3.5-turbo",
+                    "model": "gpt-3.5-turbo",
+                    "name": "GPT-3.5 Turbo",
+                    "provider": "openai",
+                    "provider_name": "OpenAI",
+                    "requires_better_ai": true,
+                    "features": [],
+                },
+                {
+                    "id": "openai-gpt-4-1106-preview",
+                    "model": "gpt-4-1106-preview",
+                    "name": "GPT-4 Turbo",
+                    "provider": "openai",
+                    "provider_name": "OpenAI",
+                    "requires_better_ai": true,
+                    "features": [],
+                },
+            ]
+            response.body['default_models'] = {
+                "chat": "openai-gpt-3.5-turbo",
+                "quick_ai": "openai-gpt-3.5-turbo",
+                "commands": "openai-gpt-3.5-turbo",
+                "api": "openai-gpt-4-1106-preview",
+            }
+            break;
     }
     response.body = JSON.stringify(response.body)
-    console.log("after request:\n" + JSON.stringify($request))
+    console.log("after request:\n" + JSON.stringify(request))
     console.log("after response:\n" + JSON.stringify(response))
     $done({
         response
     });
 } catch (error) {
-    console.log("error:" + error.message)
+    console.log("catch error : " + error.message)
     $done({});
 }
