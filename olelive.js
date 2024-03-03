a0a08d5da24257c48f51a92fe96167b63b12282e
07 <17439300+kilingzhang@users.noreply.github.com>
2024-03-04 06:04:51 +0800

    olelive.js

--- olelive.js
+++ olelive.js
@@ -3,11 +3,13 @@ let response = $response
 console.log("before request:\n" + JSON.stringify(request))
 console.log("before response:\n" + JSON.stringify(response))
 try {
-    console.log("before response.body:\n" + response.body)
-    // response.body = JSON.parse(response.body)
-    // response.body['data']['groupId'] = 3
-    // response.body['data']['userEndTime'] = 1809501275
-    // response.body = JSON.stringify(response.body)
+    const byteArray = new Uint8Array(response.body.split(','));
+    // 创建一个TextDecoder对象
+    const decoder = new TextDecoder();
+    response.body = JSON.parse(decoder.decode(byteArray))
+    response.body['data']['groupId'] = 3
+    response.body['data']['userEndTime'] = 1809501275
+    response.body = JSON.stringify(response.body)
     console.log("after request:\n" + JSON.stringify($request))
     console.log("after response:\n" + JSON.stringify(response))
     $done({
