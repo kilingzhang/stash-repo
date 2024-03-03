744ed3c07c3d9af9f0a97d8987267e2bd7d00891
07 <17439300+kilingzhang@users.noreply.github.com>
2024-03-04 05:58:32 +0800

    olelive.js

--- olelive.js
+++ olelive.js
@@ -4,10 +4,10 @@ console.log("before request:\n" + JSON.stringify(request))
 console.log("before response:\n" + JSON.stringify(response))
 try {
     console.log("before response.body:\n" + response.body)
-    response.body = JSON.parse(response.body)
-    response.body['data']['groupId'] = 3
-    response.body['data']['userEndTime'] = 1809501275
-    response.body = JSON.stringify(response.body)
+    // response.body = JSON.parse(response.body)
+    // response.body['data']['groupId'] = 3
+    // response.body['data']['userEndTime'] = 1809501275
+    // response.body = JSON.stringify(response.body)
     console.log("after request:\n" + JSON.stringify($request))
     console.log("after response:\n" + JSON.stringify(response))
     $done({
